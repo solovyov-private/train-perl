0a14b01be78912b545aa7673d964321b24329d5c
SS <43931009+solovyov-private@users.noreply.github.com>
2020-11-20 13:03:06 +0000

    format

--- DBI.pl
+++ DBI.pl
@@ -1,39 +1,38 @@
 #!/usr/bin/perl
 
-
+use Data::Dumper;
 use DBI;
 use Encode;
-use Data::Dumper;
 #use utf8;
 
 #binmode STDOUT, ":utf8";
 #binmode STDIN, ":utf8";
 
-#my $DNS = 'dbi:mysql:database=DB_NAME;host=localhost;port=3306;';
-my $DNS = 'dbi:mysql:database=DB_NAME;host=localhost;port=3306;mysql_read_default_file=/etc/my.cnf;';
-my $user = 'user';
-my $pass = 'pass';
-my$param = {
+#my($DNS)    = 'dbi:mysql:database=DB_NAME;host=localhost;port=3306;';
+my($DNS)   = 'dbi:mysql:database=DB_NAME;host=localhost;port=3306;mysql_read_default_file=/etc/my.cnf;';
+my($user)  = 'user';
+my($pass)  = 'pass';
+my($param) = {
               (
 #                mysql_enable_utf8mb4 => 1,
-#                mysql_enable_utf8 => 1,
-                 RaiseError => 1,
-                 PrintError => 1
+#                mysql_enable_utf8    => 1,
+                 RaiseError          => 1,
+                 PrintError          => 1
               )
 };
 
-my $dbh = DBI->connect($DNS, $user, $pass, $param);
+my($dbh)   = DBI->connect($DNS, $user, $pass, $param);
 #$dbh->{'mysql_enable_utf8'}             = 1;
 #$dbh->do("SET character_set_client      = utf8;");
 #$dbh->do("SET character_set_connection  = latin1;");
 $dbh->do("SET character_set_results     = utf8;");
 #$dbh->do("SET character_set_server      = latin1;");
 
-my $sth = $dbh->prepare("SELECT * FROM TABLE order by ID asc");
+my($sth)   = $dbh->prepare("SELECT * FROM TABLE order by ID asc");
 
 $sth->execute();
 
-my(@row);
+my(@row)   = ( );
 
 while(@row = $sth->fetchrow_array){
         print STDOUT Dumper \@row;
